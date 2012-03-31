455cad131ea4dda309e2067d52a712e8efb43ee7
lincoln.liu <xianlinbox@gmail.com>
2012-03-31 11:35:47 +0800

    add the Helloworld.java

---
+++ JavaFunctionCode/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lincoln.liu
+ * Date: 3/31/12
+ * Time: 11:05 AM
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello world!");
+    }
+}
